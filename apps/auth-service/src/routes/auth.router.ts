ee6667700e2edce3b283683801f32e97af43d0a7
khiemvuong <22520664@gm.uit.edu.vn>
2025-10-01 11:20:16 +0700

    feat(auth) Forgot password

--- apps/auth-service/src/routes/auth.router.ts
+++ apps/auth-service/src/routes/auth.router.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { userLogin, userRegistetration, verifyUser } from "../controller/auth.controller";
+import { resetUserPassword, userForgotPassword, userLogin, userRegistetration, verifyUser, verifyUserForgotPassword } from "../controller/auth.controller";
 
 const router:Router = express.Router();
 
@@ -7,5 +7,7 @@ const router:Router = express.Router();
 router.post("/user-registration", userRegistetration);
 router.post("/verify-user", verifyUser);
 router.post("/login-user", userLogin);
-
+router.post("/forgot-password-user", userForgotPassword);
+router.post("/reset-password-user", resetUserPassword);
+router.post("/verify-forgot-password-user", verifyUserForgotPassword);
 export default router;
